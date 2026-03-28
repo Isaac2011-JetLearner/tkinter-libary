a28db097baadce36c43fb57cdf9fa8e8b191ee2a
Isaac2011-JetLearner <oliveredosa@yahoo.com>
2026-03-28 12:17:41 +0000

    Add files via upload

--- rock-paper-scissor.py
+++ rock-paper-scissor.py
@@ -3,16 +3,59 @@ import random
 screen = tkinter.Tk()
 screen.geometry("800x400")
 screen.title("Rock-Paper-scissors")
-
+player_score = 0
+computer_score = 0
 selections = ["rock","paper","scissor"]
 def choice(user):
+    global player_score,computer_score
     computer = random.choice(selections)
     label_7.config(text= "Computer Selected :"+computer)
+    label_5.config(text="Player Selected :"+ user) 
+
+    if user == "rock" and computer == "scissor":
+        player_score+=1
+        label_6.config(text= "Player Score :"+str(player_score))
+        label_4.config(text = "You win")
     
-    label_5.config(text="Player Selected :"+ user)
+    if user == "paper" and computer == "rock":
+       player_score+=1
+       label_6.config(text= "Player Score :"+str(player_score))
+       label_4.config(text = "You win")
+
+    if user == "scissor" and computer == "paper":
+        player_score+=1
+        label_6.config(text= "Player Score :"+str(player_score))
+        label_4.config(text = "You win")
+
+    if user == "paper" and computer == "paper":
+        label_4.config(text= "Its A Draw")    
+
+    if user == "rock" and computer == "rock":
+        label_4.config(text= "Its A Draw")
+
+    if user == "scissor" and computer == "scissor":
+        label_4.config(text= "Its A Draw")
+
+    if user == "rock" and computer == "paper":
+        computer_score+=1
+        label_8.config(text= "Computer Score :"+str(computer_score))
+        label_4.config(text= "You Lose")
+
+    if user == "paper" and computer == "scissor":
+        computer_score+=1
+        label_8.config(text= "Computer Score :"+str(computer_score))
+        label_4.config(text= "You Lose")
+    
+    if user == "scissor" and computer == "rock":
+        computer_score+=1
+        label_8.config(text= "Computer Score :"+str(computer_score))
+        label_4.config(text= "You Lose")
+        
 
     
+   
 
+        
     
 
 
@@ -21,14 +64,14 @@ label_2 = tkinter.Label(screen, text= "Your Options:")
 label_3 = tkinter.Label(screen, text= "Score:")
 label_4 = tkinter.Label(screen,text= "You Won!!!!")
 label_5 = tkinter.Label(screen,text= "You Selected: ")
-label_6 = tkinter.Label(screen,text= "Player Score: ")
+label_6 = tkinter.Label(screen,text= "Player Score: " )
 label_7 = tkinter.Label(screen,text= "Computer Selected: ")
 label_8 = tkinter.Label(screen,text= "Computer Score: ")
 
 
-button_1 = tkinter.Button(screen,text= "Rock",width= 20,command= lambda:choice(user= "Rock"))
-button_2 = tkinter.Button(screen,text = "Paper",width= 20,command= lambda:choice(user= "Paper"))
-button_3 = tkinter.Button(screen,text= "Scissor",width= 20,command= lambda:choice(user= "Scissor"))
+button_1 = tkinter.Button(screen,text= "Rock",width= 20,command= lambda:choice(user= "rock"))
+button_2 = tkinter.Button(screen,text = "Paper",width= 20,command= lambda:choice(user= "paper"))
+button_3 = tkinter.Button(screen,text= "Scissor",width= 20,command= lambda:choice(user= "scissor"))
 
 label_1.grid(row= 1, column= 2,columnspan= 2)
 label_2.grid(row= 4,column= 1)
