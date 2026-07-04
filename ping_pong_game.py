039eb3d55c18658bee01923c48e1832c22bc1c46
Isaac2011-JetLearner <oliveredosa@yahoo.com>
2026-07-04 12:46:03 +0100

    Add files via upload

--- ping_pong_game.py
+++ ping_pong_game.py
@@ -1,5 +1,6 @@
 import tkinter
 import time
+import tkinter.messagebox
 screen = tkinter.Tk()
 screen.title("Ping Pong Game")
 screen.geometry("800x400")
@@ -19,6 +20,7 @@ class Ball():
         if keep[0] <= 0:
             self.speedx = 2
             self.score2 +=1
+            canvas.itemconfigure(player2_score,text = "Player 2 score : " + str(self.score2))
 
         if keep[1] <=0:
             self.speedy = 2
@@ -26,10 +28,26 @@ class Ball():
         if keep[2] >=800:
             self.speedx = -2
             self.score1 +=1
+            canvas.itemconfigure(player1_score,text = "Player 1 score : " + str(self.score1))
 
         if keep[3]>= 400:
             self.speedy = -2
 
+
+    def check_collision(self,player_1,player_2):
+        p1c = canvas.coords(player_1.player)
+        p2c = canvas.coords(player_2.player)
+        ballc = canvas.coords(self.ball)
+
+        if ballc[3] >= p1c[1] and ballc[1] <= p1c[3]:
+            if ballc[0] <= p1c[2] and ballc[0] >= p1c[0]:
+                self.speedx = 2
+
+        if ballc[3] >= p2c[1] and ballc[1] <+ p2c[3]:
+            if ballc[2] >= p2c[0] and ballc[2] <= p2c[2]:
+                self.speedx = -2
+                
+            
 
 
         
@@ -52,6 +70,7 @@ class Player():
         keep = canvas.coords(self.player)
         if keep[1] <= 0:
             self.movey = 0
+            
 
         if keep[3] >=400 :
             self.movey = 0
@@ -68,15 +87,21 @@ class Player():
 canvas = tkinter.Canvas(screen,background= "black",width= 800,height= 400)
 canvas.create_line(400,0,400,400,fill= "white",width= 3)
 canvas.create_oval(360,160,440,240,outline= "white",width= 3)
+player1_score = canvas.create_text(50,10,font=("Arial",10,"bold"),text= "Player 1 score:", fill= "white")
+player2_score = canvas.create_text(730,15,font=("Arial",10,"bold"),text= "Player 2 score:", fill= "white")
 
 
 player1 = Player(10,170,30,270,"orange")
 player1.draw()
 canvas.bind_all("w",player1.up)
+canvas.bind_all("s",player1.down)
 
 
-player2 = Player(790,170,770,270,"orange")
+player2 = Player(790,170,770,270,"light green")
 player2.draw()
+canvas.bind_all("<KeyPress-Up>",player2.up)
+canvas.bind_all("<KeyPress-Down>",player2.down)
+
 
 canvas.pack()
 
@@ -85,11 +110,15 @@ ball = Ball()
 running = True
 
 while running:
+    if ball.score1 == 10 or ball.score2 == 10:
+        tkinter.messagebox.showinfo("GAME OVER","Player 1 = "+ str(ball.score1)+ "Player 2 ="+ str(ball.score2))
+        break
     ball.movement()
     player1.move()
     player2.move()
     screen.update_idletasks()
     screen.update()
+    ball.check_collision(player1,player2)
     time.sleep(0.005)
 
 screen.mainloop()
